955e470d246a8f2cf01bdb795ef086f6b4198338
Pedro Ivo <kuhnpedroivo@gmail.com>
2023-01-10 11:07:51 -0300

    Exception handling - findById

---
+++ src/main/java/com/cursojava/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cursojava.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+    
+}
